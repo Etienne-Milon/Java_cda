e9f5071f65e57f041cc72cd6268e34ad05009032
Etienne-Milon <97611540+Etienne-Milon@users.noreply.github.com>
2022-07-26 14:36:30 +0200

    Delete maquette directory

--- maquette/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module fr.em.maquette.maquette {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires fr.em.controlledtextfield;
-    requires fr.em.secusocialtextfield;
-
-    opens fr.em.maquette.maquette to javafx.fxml;
-    exports fr.em.maquette.maquette;
-}
